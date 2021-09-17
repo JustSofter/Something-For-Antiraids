6fae43ba1ba0a7fe7d1b6cc6fcf1da86484aa28d
Softer <70171835+JustSofter@users.noreply.github.com>
2021-09-17 02:48:46 -0500

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -15,6 +15,5 @@ module.exports = {
           if(member.user.bot) return;
           await member.kick("Actijoin activado.")
         }
-      // member.send('El antijoin está activado, por lo que has sido kickeado del server. https://cdn.discordapp.com/attachments/796487933342384180/886695087051006002/image0-9-1.jpg')
     }
 }

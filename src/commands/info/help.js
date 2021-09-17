ea9234753c6a8b606c51687267ff9f6e120d21bc
Softer <70171835+JustSofter@users.noreply.github.com>
2021-09-17 03:04:31 -0500

    Update help.js

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -6,7 +6,7 @@ module.exports = {
         const embed = new MessageEmbed()
         .setTitle('Bot commands.')
         .setDescription('\`->antichannelcreate\` Disable the channel creation in the server.\n\`->antichanneldelete\` Disable channel deletion on the server. (Restores deleted channels.)\n\`->antiinvite\` Block any invites from being posted in the server.\n\`->antijoin\` Instantly Kick any member that joins.\n\`->lockdown\` Lock the Send Messages permission from every channel.\n\nCommand Usage:\n\`->command enable\` Enable the respective system.\n\`->command disable\` Disable the respective system.')
-        .setFooter(`Comando ejecutado por: ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+        .setFooter(`Command executed by: ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
         .setColor("RANDOM")
         message.channel.send({ embeds: [embed]})
     }

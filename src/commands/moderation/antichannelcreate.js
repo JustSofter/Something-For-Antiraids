2ab40b1a5a990d5bad4179db2ec7f0fc677d1636
Softer <70171835+JustSofter@users.noreply.github.com>
2021-09-17 02:31:57 -0500

    Update antichannelcreate.js

--- src/commands/moderation/antichannelcreate.js
+++ src/commands/moderation/antichannelcreate.js
@@ -3,24 +3,24 @@ module.exports = {
     name: 'antichannelcreate',
     run: async(client, message, args) => {
         if(!message.member.permissions.has("ADMINISTRATOR")) {
-            return message.reply('No tienes los permisos suficientes para ejecutar este comando.')
+            return message.reply('You don't have enough permissions to execute that command.')
         }
         if(args[0] === 'enable') {
             let id = db.get(`antichannelcreate-${message.guild.id}`);
             if(!id) {
                 db.set(`antichannelcreate-${message.guild.id}`, true);
-                return message.reply('Has desactivado la creación de canales en tu servidor.')
+                return message.reply('You just disabled Channel Creation in your server.')
             } else {
-                return message.reply('Ya desactivaste la creación de canales.')
+                return message.reply('The anti-channel creation system is already enabled.')
             }
         }
         if(args[0] === 'disable') {
             let id = db.get(`antichannelcreate-${message.guild.id}`, true);
             if(!id) {
-                return message.reply('No has activado la protección de creación de canales en tu servidor.')
+                return message.reply('You haven't enabled the Anti-channel creation system.')
             } else {
                 db.set(`antichannelcreate-${message.guild.id}`, false);
-                return message.reply('Has desactivado la protección de creación de canales.')
+                return message.reply('You just enabled Channel Creation in your server.')
             }
         }
         
